6c391e19d29c0c87192d7ed7d93dabc4ce799349
fusion809 <brentonhorne77@gmail.com>
2016-05-13 21:51:22 +1000

    Updating

--- lib/ubuntu-build.sh
+++ lib/ubuntu-build.sh
@@ -6,7 +6,7 @@ function ubuntu-build {
   curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
   sudo apt-get install -y nodejs build-essential git libudev0 \
     libgnome-keyring-dev fakeroot python libgcrypt11-dev \
-    libgtk2-dev
+    libgtk2.0-dev
   sudo npm install -g node-gyp
   brackets-build
 }
