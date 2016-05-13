cd9183df1bfefd74bc4074f2d8aac89b3399357f
fusion809 <brentonhorne77@gmail.com>
2016-05-14 01:44:09 +1000

    Updating

--- lib/sabayon-build.sh
+++ lib/sabayon-build.sh
@@ -4,9 +4,9 @@
 
 function sabayon-build {
   # Get dependencies
-  sudo equo i net-libs/nodejs sys-devel/base-gcc sys-devel/gcc sys-libs/libstdc++ sys-libs/glibc \
+  sudo equo i net-libs/nodejs sys-devel/base-gcc sys-devel/gcc virtual/libstdc++ sys-libs/glibc \
     dev-vcs/git gnome-base/gnome-keyring gnome-base/libgnome-keyring x11-libs/libX11 dev-libs/libgcrypt \
-    sys-apps/usbutils sys-apps/kmod dev-util/gperf dev-lang/python:2.7 x11-libs/gtk+:2
+    sys-apps/usbutils sys-apps/kmod dev-util/gperf dev-lang/python:2.7 x11-libs/gtk+:2 virtual/libudev
   if ! `comex node-gyp`; then
     sudo npm install -g node-gyp
   fi
