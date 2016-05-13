3f458dcace8a419343f74c6554feb2f6e01f5fe0
fusion809 <brentonhorne77@gmail.com>
2016-05-13 23:17:45 +1000

    Updating

--- lib/opensuse-build.sh
+++ lib/opensuse-build.sh
@@ -3,10 +3,16 @@
 
 function opensuse-build {
   # Get dependencies
+  OSVER=$(lsb_release -r | sed 's/Release:\s*//g')
+  if [[ $OSVER == "42.1" ]]; then
+    sudo zypper in -y http://download.opensuse.org/repositories/home:/simotek:/base/openSUSE_Leap_42.1/x86_64/libgcrypt11-1.5.4-7.1.x86_64.rpm \
+    http://download.opensuse.org/repositories/home:/etrash/openSUSE_Leap_42.1/x86_64/libudev0-182-1.1.x86_64.rpm
+  fi
   sudo zypper in -y nodejs nodejs-devel make gcc gcc-c++ glibc-devel \
-    git-core libgnome-keyring-devel libX11-devel libudev0 libgcrypt \
-    gtk2-devel python
-  sudo npm install -g node-gyp
+    git-core libgnome-keyring-devel libX11-devel gtk2-devel python
+  if ! `comex node-gyp`; then
+    sudo npm install -g node-gyp
+  fi
   brackets-build
 }
 
