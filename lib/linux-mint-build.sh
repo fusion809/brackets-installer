dbbc78d76b6726f1f6187d5f6757742b7b0f1012
fusion809 <brentonhorne77@gmail.com>
2016-05-13 21:58:54 +1000

    Updating

--- lib/linux-mint-build.sh
+++ lib/linux-mint-build.sh
@@ -4,7 +4,7 @@ function linux-mint-build {
   # Get dependencies
   sudo apt-get install -y curl
   curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-  sudo apt-get install -y nodejs build-essential git libudev0 \
+  sudo apt-get install -y nodejs build-essential git \
     libgnome-keyring-dev fakeroot python libgcrypt11-dev \
     libgtk2.0-dev
   sudo npm install -g node-gyp
--- x86_64/debian.sh
+++ x86_64/debian.sh
@@ -15,6 +15,14 @@ if [[ $preference == "A" ]]; then
 
 elif [[ $preference == "B" ]]; then
 
+  if ! [[ -d $HOME/Programs ]]; then
+    mkdir $HOME/Programs
+  fi
+  cd $HOME/Programs
+  curl -OsL http://ftp.au.debian.org/debian/pool/main/libg/libgcrypt11/libgcrypt11_1.5.0-5+deb7u4_amd64.deb
+  curl -OsL http://ftp.au.debian.org/debian/pool/main/u/udev/libudev0_175-7.2_amd64.deb
+  sudo dpkg -i *.deb
+  sudo apt-get -f install
   debian-build
 
 fi
