f336b8f1aa1f98459834edb017856aba57f68364
fusion809 <brentonhorne77@gmail.com>
2016-05-13 15:07:03 +1000

    Updating

---
+++ i686/fedora.sh
@@ -0,0 +1,14 @@
+printf "Do you want to install Brackets from A) a binary package (faster) or would you prefer B) a source install (slower)? [A/B] "
+read preference
+
+if [[ $preference == "A" ]]; then
+
+  sudo dnf copr enable mosquito/brackets
+  sudo dnf update
+  sudo dnf install -y brackets
+
+elif [[ $preference == "B" ]]; then
+
+  fedora-build
+
+fi
---
+++ lib/fedora-build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+. ./lib/brackets-build.sh
+
+function fedora-build {
+  # Get dependencies
+  sudo dnf copr enable nibbler/nodejs
+  sudo dnf copr enable mosquito/brackets
+  sudo dnf update -y
+  sudo dnf install -y unzip nodejs-devel gcc gcc-c++ make \
+    glibc-devel git-core compat-libgcrypt
+  brackets-build
+}
+
+export -f fedora-build
