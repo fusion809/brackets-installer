fb93cfd1d1a5c29963f73ef2ee2a1601a8cdb2d2
fusion809 <brentonhorne77@gmail.com>
2016-05-13 19:27:40 +1000

    Updating

--- lib/debian-build.sh
+++ lib/debian-build.sh
@@ -4,8 +4,16 @@ function debian-build {
   # Get dependencies
   sudo apt-get install -y curl
   curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-  sudo apt-get install -y nodejs build-essential git libudev-dev \
-    libgnome-keyring-dev fakeroot libgcrypt20-dev libgtk2.0-dev python
+  sudo apt-get install -y nodejs build-essential git \
+    libgnome-keyring-dev fakeroot libgtk2.0-dev python
+  if ! [[ -d $HOME/Programs ]]; then
+    mkdir $HOME/Programs
+  fi
+  cd $HOME/Programs
+  curl -OsL http://ftp.au.debian.org/debian/pool/main/libg/libgcrypt11/libgcrypt11_1.5.0-5+deb7u4_amd64.deb
+  curl -OsL http://ftp.au.debian.org/debian/pool/main/u/udev/libudev0_175-7.2_amd64.deb
+  sudo dpkg -i *.deb
+  sudo apt-get -f install
   if ! `comex node-gyp`; then
     sudo npm install -g node-gyp
   fi
