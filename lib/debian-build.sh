8393972eea5dc14adca4498688dbb61ed1b3f596
fusion809 <brentonhorne77@gmail.com>
2016-05-13 17:55:09 +1000

    Updating

--- lib/debian-build.sh
+++ lib/debian-build.sh
@@ -5,7 +5,7 @@ function debian-build {
   sudo apt-get install -y curl
   curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
   sudo apt-get install -y nodejs build-essential git libudev-dev \
-    libgnome-keyring-dev fakeroot libx11-dev libgcrypt20-dev libgtk2.0-dev python
+    libgnome-keyring-dev fakeroot libgcrypt20-dev libgtk2.0-dev python
   sudo npm install -g node-gyp
   brackets-build
 }
