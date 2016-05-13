b85abce64bac1e63180494bf7ef1d1b23626d0b0
fusion809 <brentonhorne77@gmail.com>
2016-05-13 16:16:39 +1000

    Updating

--- lib/centos-build.sh
+++ lib/centos-build.sh
@@ -6,7 +6,7 @@ function centos-build {
   # Get dependencies
   curl -sL https://rpm.nodesource.com/setup_6.x | sudo bash -     # Get the latest version of Node.js
   sudo yum install -y unzip nodejs-devel gcc gcc-c++ make \
-    glibc-devel git-core libgcrypt gtk2-devel python
+    glibc-devel git-core libgcrypt gtk2-devel python libblkid
   sudo npm install -g node-gyp
   libudev0-build
   brackets-build
